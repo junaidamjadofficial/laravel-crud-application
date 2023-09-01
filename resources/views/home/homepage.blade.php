15de4a70095b6b426d17e0fa01a0bdda83002df5
Junaid Amjad <junaid0071@outlook.com>
2023-09-01 11:55:51 +0500

    First Commit

---
+++ resources/views/home/homepage.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+     @include('home.homecss')
+   </head>
+   <body>
+      <!-- header section start -->
+      <div class="header_section">
+         @include('home.header')
+         <!-- banner section start -->
+         @include('home.banner')
+         <!-- banner section end -->
+      </div>
+      <!-- header section end -->
+      <!-- services section start -->
+         @include('home.service')
+      <!-- services section end -->
+      <!-- about section start -->
+         @include('home.about')
+      <!-- about section end -->
+      <!-- blog section start -->
+      
+      <!-- blog section end -->
+      
+      <!-- client section start -->
+      <!-- choose section start -->
+      
+      <!-- choose section end -->
+      <!-- footer section start -->
+      @include('home.footer')
+      <!-- footer section end -->
+      <!-- copyright section start -->
+      
+   </body>
+</html>
